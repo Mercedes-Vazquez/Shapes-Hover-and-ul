4ce47fb5961b756a05df78123e9695624228e97e
Mercedes <mvazquezsan@gmail.com>
2021-01-03 23:24:16 +0100

    functions in js added still not fully functional, one more button in HTML, and styling of buttons

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // curveVertex(mouseX, mouseY);
 // MouseEvent
+'use strict';
+
+let southAmericanMeals = ["Gallo Pinto","Arepas","Pati","Chorreadas","Patacones","Casado"];
+let spanishMeals = ["Paella","Tortilla de Patatas","Patatas Bravas","Calamares en su Tinta","Fricandó","Caracoles a la Llauna"];
+
+// function show() {
+//     let x = southAmericanMeals.toString();
+//     document.getElementById("list1").innerHTML = x;
+// }
+
+function removeCostaRicanElement() {
+    
+    document.getElementById("list1").innerHTML = southAmericanMeals;
+    southAmericanMeals.splice(0, 1);
+}
+function removeSpanishElement() {
+    
+    document.getElementById("list2").innerHTML = spanishMeals;
+    southAmericanMeals.splice(0, 1);
+}
