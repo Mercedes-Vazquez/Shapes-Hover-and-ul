fc2aadebdd4b2e67653f912c5be02cf9b418b7ba
Mercedes <mvazquezsan@gmail.com>
2021-01-04 13:21:59 +0100

    centering buttons in css

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// curveVertex(mouseX, mouseY);
-// MouseEvent
 'use strict';
 
 let southAmericanMeals = ["Gallo Pinto","Arepas","Pati","Chorreadas","Patacones","Casado"];
 let spanishMeals = ["Paella","Tortilla de Patatas","Patatas Bravas","Calamares en su Tinta","Fricandó","Caracoles a la Llauna"];
 
-// function show() {
-//     let x = southAmericanMeals.toString();
-//     document.getElementById("list1").innerHTML = x;
-// }
 
 function removeCostaRicanElement() {
     
@@ -18,5 +12,5 @@ function removeCostaRicanElement() {
 function removeSpanishElement() {
     
     document.getElementById("list2").innerHTML = spanishMeals;
-    southAmericanMeals.splice(0, 1);
+    spanishMeals.splice(0, 1);
 }
